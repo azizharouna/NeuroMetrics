5a1a3d2f8c58a363273859fac7a06fc6a6cf7105
Aziz <abdoulenergy@gmail.com>
2023-10-15 17:36:29 +0200

    baseline model selection

--- utilspro.py
+++ utilspro.py
@@ -6,6 +6,12 @@ import seaborn as sns
 from sklearn.covariance import MinCovDet
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.metrics import mean_squared_error
 
 #reading the data 
 # Unzipping the provided dataset
@@ -17,3 +23,17 @@ train_peptides = pd.read_csv("data/amp-parkinsons-disease-progression-prediction
 train_proteins = pd.read_csv("data/amp-parkinsons-disease-progression-prediction_2/train_proteins.csv")
 train_clinical_data = pd.read_csv("data/amp-parkinsons-disease-progression-prediction_2/train_clinical_data.csv")
 supplemental_clinical_data = pd.read_csv("data/amp-parkinsons-disease-progression-prediction_2/supplemental_clinical_data.csv")
+
+
+
+
+def execute_notebook(notebook_filename):
+    with open(notebook_filename, "r", encoding="utf-8") as f:
+        notebook = nbformat.read(f, as_version=4)
+
+    ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
+
+    ep.preprocess(notebook, {'metadata': {'path': './'}})
+
+    with open(notebook_filename, "w", encoding="utf-8") as f:
+        nbformat.write(notebook, f)
