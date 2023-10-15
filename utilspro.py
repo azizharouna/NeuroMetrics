246f12d341d902920f80667b48b57643603e5142
Aziz <abdoulenergy@gmail.com>
2023-10-15 15:53:02 +0200

    data preprocessing - imputed to clinical data

--- utilspro.py
+++ utilspro.py
@@ -2,3 +2,14 @@ import zipfile
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+#reading the data 
+# Unzipping the provided dataset
+with zipfile.ZipFile("data/amp-parkinsons-disease-progression-prediction_2.zip", 'r') as zip_ref:
+    zip_ref.extractall("data/amp-parkinsons-disease-progression-prediction_2")
+
+# Load each dataset into a dataframe
+train_peptides = pd.read_csv("data/amp-parkinsons-disease-progression-prediction_2/train_peptides.csv")
+train_proteins = pd.read_csv("data/amp-parkinsons-disease-progression-prediction_2/train_proteins.csv")
+train_clinical_data = pd.read_csv("data/amp-parkinsons-disease-progression-prediction_2/train_clinical_data.csv")
+supplemental_clinical_data = pd.read_csv("data/amp-parkinsons-disease-progression-prediction_2/supplemental_clinical_data.csv")
